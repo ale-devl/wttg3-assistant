852d685f4d3de931a29ce99463189faffcc673e3
otrexdev <otrexdev@gmail.com>
2026-07-17 05:25:50 +0200

    Clickpoints and Opening Times update

--- Clickpoint Guides/Assets/100_audioPlay.js
+++ Clickpoint Guides/Assets/100_audioPlay.js
@@ -1,13 +1,13 @@
 const bizFile = [
-    "Assets/100_clippedTransmission.wav",
-    "Assets/100_creatureTalk.wav",
-    "Assets/100_farmStrange.wav",
-    "Assets/100_hypnoLoop.wav",
-    "Assets/100_internalsWave.wav",
-    "Assets/100_pumpFreq.wav",
-    "Assets/100_trafficHum.wav",
-    "Assets/100_trainDrone.wav",
-    "Assets/100_unknownIdle.wav"
+    "sfx/clippedTransmission.wav",
+    "sfx/creatureTalk.wav",
+    "sfx/farmStrange.wav",
+    "sfx/hypnoLoop.wav",
+    "sfx/internalsWave.wav",
+    "sfx/pumpFreq.wav",
+    "sfx/trafficHum.wav",
+    "sfx/trainDrone.wav",
+    "sfx/unknownIdle.wav"
 ];
 
 const bizSounds = [
@@ -26,16 +26,16 @@ function selectAudio(event){
 	stopTrack();
 	if(bizSounds.includes(event.id)){
 		document.getElementById("selection").innerHTML = "Selection " + event.id;
-        
+        sendEventToUnreal("BPSelectAudio", {TrackName: event.id});
 	}else{
 		document.getElementById("selection").innerHTML = "Select sound";
 	}
 }
 
 function playTrack() {
-    
+    sendEventToUnreal("PlayAudio", {});
 }
 
 function stopTrack() {
-    
+    sendEventToUnreal("StopAudio", {});
 }
